ae07fa8a2c27fb986cea45713c63a798a2b8ba4e
AbednegoTenge <abednegotenge180@gmail.com>
2026-01-28 09:57:08 +0000

    notification on applications

--- core/signals.py
+++ core/signals.py
@@ -1,6 +1,6 @@
 from django.db.models.signals import post_save
 from django.dispatch import receiver
-from .models import CandidateProfile, EmployerProfile, Address
+from .models import CandidateProfile, EmployerProfile, Address, Notification, Application
 from django.contrib.auth import get_user_model
 
 User = get_user_model()
@@ -13,3 +13,24 @@ def create_candidate_or_employer_profile(sender, instance, created, **kwargs):
     elif created and instance.is_employer:
         EmployerProfile.objects.get_or_create(user=instance)
         Address.objects.get_or_create(user=instance)
+
+
+@receiver(post_save, sender=Application)
+def send_notification(sender, instance, created, **kwargs):
+    if created:
+        Notification.objects.create(
+            user=instance.job.employer.user,
+            title=f"New Application for {instance.job.title}",
+            notification_type=Notification.NotificationType.APPLICATION,
+            content=f"{instance.user.get_full_name()} has applied for the position of {instance.job.title}.",
+        )
+
+@receiver(post_save, sender=Application)
+def send_notification(sender, instance, created, **kwargs):
+    if created:
+        Notification.objects.create(
+            user=instance.user,
+            title=f"Application for {instance.job.title} has been submitted",
+            notification_type=Notification.NotificationType.APPLICATION,
+            content=f"You have successfully applied for the position of {instance.job.title} at {instance.job.employer.company_name}.",
+        )
