bff41c42ed995d01feef7b22cc46b7458e3b73d5
AbednegoTenge <abednegotenge180@gmail.com>
2026-02-03 21:22:15 +0000

    fixing s3 path

--- core/static_backend.py
+++ core/static_backend.py
@@ -1,12 +1,10 @@
 from storages.backends.s3boto3 import  S3Boto3Storage
 
 class PublicMediaStorage(S3Boto3Storage):
-    location = 'images'
     default_acl = 'public-read'
     file_overwrite = False
 
 class PrivateMediaStorage(S3Boto3Storage):
-    location = 'resumes'
     default_acl = 'private'
     file_overwrite = False
     custom_domain = False
