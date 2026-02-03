957c9e9d73dd0f61bd54d68b9c7d34700fa835b3
AbednegoTenge <abednegotenge180@gmail.com>
2026-02-03 21:33:49 +0000

    s3

--- core/static_backend.py
+++ core/static_backend.py
@@ -3,6 +3,7 @@ from storages.backends.s3boto3 import  S3Boto3Storage
 class PublicMediaStorage(S3Boto3Storage):
     default_acl = 'public-read'
     file_overwrite = False
+    custom_domain = False
 
 class PrivateMediaStorage(S3Boto3Storage):
     default_acl = 'private'
