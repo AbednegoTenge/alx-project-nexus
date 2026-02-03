5caf7567a85e26678ac6d5dd184289b00dd7bf18
AbednegoTenge <abednegotenge180@gmail.com>
2026-02-03 19:52:56 +0000

    settings

--- jobboard/settings.py
+++ jobboard/settings.py
@@ -120,7 +120,14 @@ SIMPLE_JWT = {
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
 DATABASES = {
-    'default': dj_database_url.parse(os.getenv('DB_URL'))
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': os.getenv('DB_NAME'),
+        'USER': os.getenv('DB_USER'),
+        'PASSWORD': os.getenv('DB_PASSWORD'),
+        'HOST': os.getenv('DB_HOST'),
+        'PORT': os.getenv('DB_PORT'),
+    }
 }
 
 AUTH_USER_MODEL = 'core.User'
